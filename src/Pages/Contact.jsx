62422cdc2a7eb62444638b5eb7065cf79753c378
Ayushi Jain <97507160+ayushi8855@users.noreply.github.com>
2023-03-31 15:12:38 +0530

    Update Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import Address from '../Components/Address/Address'
-import Form from '../Components/Form'
+
 
 
 function Contact() {
     return (
         <Container id="contact" >
          <Address/>
-         <Form/>
+        
       </Container>
     )
 }
